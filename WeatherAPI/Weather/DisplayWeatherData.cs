3ca028a9029895b074c56f5b0c2f2bb603a785f8
Jansen Magbojos <Jansen.Magbojos@PH.GT.COM>
2024-06-27 23:26:00 +0800

    inherit class

---
+++ WeatherAPI/Weather/DisplayWeatherData.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using System.Xml.Serialization;
+using static WeatherAPI.Models.JsonResultModel;
+using static WeatherAPI.Models.XmlResultModel;
+
+namespace WeatherAPI.Weather
+{
+    public class DisplayWeatherData
+    {
+        protected static void PrintXmlResult(string result)
+        {
+            Current weatherResponse = DeserializeXml(result);
+            //sample fields to be displayed
+            Console.WriteLine($"Location: {weatherResponse.City.Name}, {weatherResponse.City.Country}");
+            Console.WriteLine($"Temperature: {weatherResponse.Temperature.Value} {weatherResponse.Temperature.Unit}");
+            Console.WriteLine($"Weather: {weatherResponse.Weather.Value}");
+        }
+        protected static void PrintJsonResult(string result)
+        {
+            WeatherResponse? deserialized = DeserializeJson(result);
+            //sample fields to be displayed
+            Console.WriteLine($"Location: {deserialized.name}, {deserialized.sys.country}");
+            Console.WriteLine($"Temperature: {deserialized.main.temp}K");
+            Console.WriteLine($"Weather: {deserialized.weather[0].description}");
+        }
+        private static WeatherResponse? DeserializeJson(string jsonResult)
+        {
+            return JsonSerializer.Deserialize<WeatherResponse>(jsonResult);
+        }
+        private static Current DeserializeXml(string xml)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Current));
+            using StringReader reader = new StringReader(xml);
+            return (Current)serializer.Deserialize(reader);
+        }
+    }
+}
--- WeatherAPI/Weather/Weather.cs
+++ WeatherAPI/Weather/Weather.cs
@@ -1,11 +1,6 @@
-﻿using System.Text.Json;
-using System.Xml.Serialization;
-using static WeatherAPI.Models.JsonResultModel;
-using static WeatherAPI.Models.XmlResultModel;
-
-namespace WeatherAPI.Weather
+﻿namespace WeatherAPI.Weather
 {
-    public static class Weather
+    public class Weather : DisplayWeatherData
     {
         public static async Task GetDetails(string url)
         {
@@ -13,7 +8,6 @@ namespace WeatherAPI.Weather
             try
             {
                 HttpResponseMessage response = await httpClient.GetAsync(url);
-
                 // Ensure a successful response
                 if (response.IsSuccessStatusCode)
                 {
@@ -41,33 +35,5 @@ namespace WeatherAPI.Weather
                 Console.WriteLine($"Error fetching content from the URL: {ex.Message}");
             }
         }
-        #region PRIVATE METHODS
-        private static void PrintXmlResult(string result)
-        {
-            Current weatherResponse = DeserializeXml(result);
-            //sample fields to be displayed
-            Console.WriteLine($"Location: {weatherResponse.City.Name}, {weatherResponse.City.Country}");
-            Console.WriteLine($"Temperature: {weatherResponse.Temperature.Value} {weatherResponse.Temperature.Unit}");
-            Console.WriteLine($"Weather: {weatherResponse.Weather.Value}");
-        }
-        private static void PrintJsonResult(string result)
-        {
-            WeatherResponse? deserialized = DeserializeJson(result);
-            //sample fields to be displayed
-            Console.WriteLine($"Location: {deserialized.name}, {deserialized.sys.country}");
-            Console.WriteLine($"Temperature: {deserialized.main.temp}K");
-            Console.WriteLine($"Weather: {deserialized.weather[0].description}");
-        }
-        private static WeatherResponse? DeserializeJson(string jsonResult)
-        {
-            return JsonSerializer.Deserialize<WeatherResponse>(jsonResult);
-        }
-        private static Current DeserializeXml(string xml)
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(Current));
-            using StringReader reader = new StringReader(xml);
-            return (Current)serializer.Deserialize(reader);
-        }
-        #endregion
     }
 }
