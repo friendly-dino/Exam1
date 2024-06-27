8a7415c149c86e679f0b5eb29c73662d061fb48e
Jansen Magbojos <Jansen.Magbojos@PH.GT.COM>
2024-06-27 17:02:27 +0800

    call the weather methods

--- WeatherAPI/Program.cs
+++ WeatherAPI/Program.cs
@@ -1,16 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-
 using WeatherAPI;
 
 try
 {
-
-    await Weather.GetJsonDetails(Constant.jsonResultURL + Constant.APIKey);
-
+    Console.WriteLine("Deserialized JSON data: \n");
+    await Weather.GetDetails(Constant.jsonResultURL + Constant.APIKey, ResultFormat.Json);
+    Console.WriteLine("\nDeserialized XML data: \n");
+    await Weather.GetDetails(Constant.xmlResultURL + Constant.APIKey, ResultFormat.Xml);
 }
-catch (Exception)
+catch (Exception ex)
 {
-
-    throw;
+    throw ex;
 }
