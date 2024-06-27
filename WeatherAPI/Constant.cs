3c6b41a7be9059872287d68c39b6e0553bea01d3
Jansen Magbojos <Jansen.Magbojos@PH.GT.COM>
2024-06-27 23:15:44 +0800

    use manila

--- WeatherAPI/Constant.cs
+++ WeatherAPI/Constant.cs
@@ -3,7 +3,7 @@
     public static class Constant
     {
         public const string APIKey = "75206d4e15ec66e4ee6237b8ca8bf0c4";
-        public const string jsonResultURL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=";
-        public const string xmlResultURL = "https://api.openweathermap.org/data/2.5/weather?q=London&mode=xml&appid=";
+        public const string jsonResultURL = "https://api.openweathermap.org/data/2.5/weather?q=manila&appid=";
+        public const string xmlResultURL = "https://api.openweathermap.org/data/2.5/weather?q=manila&mode=xml&appid=";
     }
 }
