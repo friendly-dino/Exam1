b74de8dd9c139ff8eb0ec2811b9c64fb4989201e
Jansen Magbojos <Jansen.Magbojos@PH.GT.COM>
2024-06-27 16:26:29 +0800

    create handling for json request

---
+++ WeatherAPI/Weather.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using static WeatherAPI.JsonResultModel;
+
+namespace WeatherAPI
+{
+    public static class Weather
+    {
+        public static async Task GetJsonDetails(string url)
+        {
+            using var httpClient = new HttpClient();
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+
+                // Ensure a successful response
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResult = await response.Content.ReadAsStringAsync();
+
+                    var deserialized = JsonSerializer.Deserialize<WeatherResponse>(jsonResult);
+
+                    //this is sample print
+                    Console.WriteLine($"Location: {deserialized.name}, {deserialized.sys.country}");
+                    Console.WriteLine($"Temperature: {deserialized.main.temp}K");
+                    Console.WriteLine($"Weather: {deserialized.weather[0].description}");
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode}");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error fetching content from the URL: {ex.Message}");
+            }
+
+
+
+        }
+
+    }
+}
