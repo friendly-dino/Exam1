f94a59a47d1e3991967d919b6ef22fc69203b068
Jansen Magbojos <Jansen.Magbojos@PH.GT.COM>
2024-06-27 16:26:48 +0800

    create constant class to hold the api key

---
+++ WeatherAPI/Constant.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherAPI
+{
+    public static class Constant
+    {
+        public const string APIKey = "75206d4e15ec66e4ee6237b8ca8bf0c4";
+        public const string jsonResultURL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=";
+        public const string xmlResultURL = "";
+    }
+}
